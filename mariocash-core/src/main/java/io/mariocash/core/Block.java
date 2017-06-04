6f9123a4598397f46c138abdc5a9c2a0ebd0f674
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2017-06-04 06:03:12 +0000

    validate blockchain

--- mariocash-core/src/main/java/io/mariocash/core/Block.java
+++ mariocash-core/src/main/java/io/mariocash/core/Block.java
@@ -1,49 +1,68 @@
 package dev.zhihexireng.core;
 
-import dev.zhihexireng.core.BlockHeader;
-import dev.zhihexireng.core.Transactions;
+import java.util.Objects;
 
-import java.io.Serializable;
+public class Block implements Cloneable {
+    Long index;
+    String hash;
+    String previousHash;
+    Long timestamp;
+    String data;
 
-public class Block implements Serializable {
-
-    // Header
-    private BlockHeader header;
-
-    // Data
-    private Transactions data;
-
-
-    // Constructor
-
-    public Block(BlockHeader header, Transactions data) {
-        this.header = header;
+    public Block(Long index, String previousHash, Long timestamp, String data) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
         this.data = data;
+        this.hash = calculateHash();
     }
 
-//    public Block() {
-//        this.index = getNextBlockIndex();
-//        this.timestamp = getCurrentTime();
-//        this.pre_block_hash = getPreBlockHash();
-//        this.author = getAccount();
-//
-//        this.transactions = getNewTransactions();
-//        this.merkle_root = getMerkleRoot();
-//        this.data_size = this.transactions.size();
-//        this.signature = getSignature();
-//    }
-
-
-    // Method
-
-
-    public boolenan makeBlock(byte[] header, Transactions data){
-
-
-
-        return ture;
+    public Long nextIndex() {
+        return this.index + 1;
     }
 
+    public String calculateHash() {
+        return HashUtils.hashString(mergeData());
+    }
 
+    public String mergeData() {
+        return index + previousHash + timestamp + data;
+    }
 
+    void setData(String data) {
+        this.data = data;
+        this.hash = calculateHash();
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "index=" + index +
+                ", hash='" + hash + '\'' +
+                ", previousHash='" + previousHash + '\'' +
+                ", timestamp=" + timestamp +
+                ", data='" + data + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Block block = (Block) o;
+        return Objects.equals(index, block.index) &&
+                Objects.equals(hash, block.hash) &&
+                Objects.equals(previousHash, block.previousHash) &&
+                Objects.equals(timestamp, block.timestamp) &&
+                Objects.equals(data, block.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, hash, previousHash, timestamp, data);
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
--- mariocash-core/src/main/java/io/mariocash/core/BlockChain.java
+++ mariocash-core/src/main/java/io/mariocash/core/BlockChain.java
@@ -3,23 +3,39 @@ package dev.zhihexireng.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 
 public class BlockChain {
     private static final Logger log = LoggerFactory.getLogger(BlockChain.class);
 
     Block previousBlock;
+    Block genesisBlock;
 
     LinkedHashMap<String, Block> blocks = new LinkedHashMap<>();
 
     public void addBlock(Block newBlock) {
-        if(!isValidNewBlock(newBlock)) return;
+        if(isGenesisBlock(newBlock)) {
+            try {
+                this.genesisBlock = (Block) newBlock.clone();
+            } catch (CloneNotSupportedException e) {
+                e.printStackTrace();
+                log.warn("{}", e);
+            }
+        } else if(!isValidNewBlock(previousBlock, newBlock)) {
+            //TODO Exception 날려야 한다.
+            return;
+        }
 
         blocks.put(newBlock.hash, newBlock);
         this.previousBlock = newBlock;
     }
 
-    private boolean isValidNewBlock(Block newBlock) {
+    private boolean isGenesisBlock(Block newBlock) {
+        return genesisBlock == null && previousBlock == null && newBlock.index == 0;
+    }
+
+    private boolean isValidNewBlock(Block previousBlock, Block newBlock) {
         if (previousBlock == null) return true;
 
         if (previousBlock.index + 1 != newBlock.index) {
@@ -38,5 +54,37 @@ public class BlockChain {
 
     public int size() {
         return blocks.size();
+    }
+
+    public boolean isValidChain() {
+        Iterator<String> iterator = blocks.keySet().iterator();
+        Block firstBlock = blocks.get(iterator.next());
+        if (!this.genesisBlock.equals(firstBlock)) return false;
+
+        Block previousBlock = firstBlock;
+        Block nextBlock;
+        while (iterator.hasNext()) {
+            nextBlock = blocks.get(iterator.next());
+            if (!isValidNewBlock(previousBlock, nextBlock)) {
+                return false;
+            }
+            previousBlock = nextBlock;
+        }
+
+
+        return true;
+    }
+
+    public Block getBlockByIndex(int index) {
+        Iterator<String> iterator = blocks.keySet().iterator();
+        for(int i = 0; i < index; i++) {
+            iterator.next();
+        }
+        String key = iterator.next();
+        return blocks.get(key);
+    }
+
+    public Block getBlockByHash(String hash) {
+        return blocks.get(hash);
     }
 }
