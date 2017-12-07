b1f8c9d68dad7c03845a0f3c0dd1691a6dee12bc
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2017-12-07 09:17:39 +0000

    change name of test methods

--- mariocash-node/src/test/java/io/mariocash/node/TransactionControllerTest.java
+++ mariocash-node/src/test/java/io/mariocash/node/TransactionControllerTest.java
@@ -20,31 +20,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(PeerController.class)
+@WebMvcTest(TransactionController.class)
 @Import(TestConfig.class)
-public class PeerControllerTest {
+public class TransactionControllerTest {
+    private static final Logger log = LoggerFactory.getLogger(TransactionControllerTest.class);
+
     @Autowired
     private MockMvc mockMvc;
 
-    private JacksonTester<PeerDto> json;
+    private JacksonTester<TransactionDto> json;
 
     @Before
     public void setUp() {
@@ -52,32 +54,39 @@ public class PeerControllerTest {
     }
 
     @Test
-    public void shouldBeAddPeer() throws Exception {
-        requestPeerPost(new PeerDto("127.0.0.1", 8080))
+    public void shouldBeGetTransactionByHash() throws Exception {
+        // 트랜잭션 풀에 있는 트랜잭션을 조회 후 블록 내 트랜잭션 조회 로직 추가 필요.
+        TransactionDto req = new TransactionDto();
+        req.setFrom("Dezang");
+        req.setData("transaction data");
+
+        MockHttpServletResponse response = mockMvc.perform(post("/txs")
+                .contentType(MediaType.APPLICATION_JSON).content(json.write(req).getJson()))
+                .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$.host", equalTo("127.0.0.1")))
-                .andExpect(jsonPath("$.port", equalTo(8080)));
+                .andReturn().getResponse();
+
+        String txHash = json.parseObject(response.getContentAsString()).getTxHash();
+
+        MockHttpServletResponse findRes = mockMvc.perform(get("/txs/" + txHash))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn().getResponse();
+
+        log.debug(findRes.getContentAsString());
     }
 
     @Test
-    public void shouldBeGetPeers() throws Exception {
-        requestPeerPost(new PeerDto("127.0.0.1", 8080));
-        requestPeerPost(new PeerDto("30.30.30.30", 8080));
+    public void shouldBeAddAtTransactionPool() throws Exception {
+        TransactionDto req = new TransactionDto();
+        req.setData("Dezang");
 
-        mockMvc
-                .perform(
-                        get("/peers"))
+        MockHttpServletResponse response = mockMvc.perform(post("/txs")
+                .contentType(MediaType.APPLICATION_JSON).content(json.write(req).getJson()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andDo(print());
-    }
+                .andDo(print())
+                .andReturn().getResponse();
 
-    private ResultActions requestPeerPost(PeerDto peerDto) throws Exception {
-        return mockMvc
-                .perform(
-                        post("/peers")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(json.write(peerDto).getJson()))
-                .andExpect(status().isOk());
+        assertThat(response.getContentAsString()).contains("Dezang");
     }
 }
