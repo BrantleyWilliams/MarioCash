137e3712f51a5db09e244b001ca70d1ca7049364
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2018-12-05 15:10:13 +0000

    XFTableAgent

--- mariocash-node/src/main/java/dev.zhihexireng/node/api/BlockApiImpl.java
+++ mariocash-node/src/main/java/dev.zhihexireng/node/api/BlockApiImpl.java
@@ -2,10 +2,10 @@ package dev.zhihexireng.node.api;
 
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 import dev.zhihexireng.core.NodeManager;
+import dev.zhihexireng.node.exception.InternalErrorException;
+import dev.zhihexireng.node.exception.NonExistObjectException;
 import dev.zhihexireng.node.mock.BlockMock;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 @AutoJsonRpcServiceImpl
@@ -19,26 +19,42 @@ public class BlockApiImpl implements BlockApi {
 
     @Override
     public int blockNumber() {
-        return 0;
+        try {
+            return 0;
+        } catch (Exception exception) {
+            throw new InternalErrorException();
+        }
     }
 
     @Override
-    public String getBlockByHash(String address, String tag) throws IOException {
-        //todo: getBlockByNumber
-        BlockMock blockMock = new BlockMock(nodeManager);
-        return blockMock.retBlockMock();
+    public String getBlockByHash(String address, String tag) {
+        try {
+            //todo: getBlockByNumber
+            BlockMock blockMock = new BlockMock(nodeManager);
+            return blockMock.retBlockMock();
+        } catch (Exception exception) {
+            throw new NonExistObjectException("block");
+        }
     }
 
     @Override
-    public String getBlockByNumber(String hashOfBlock, Boolean bool) throws IOException {
-        //todo: getBlockByNumber
-        BlockMock blockMock = new BlockMock(nodeManager);
-        return blockMock.retBlockMock();
+    public String getBlockByNumber(String hashOfBlock, Boolean bool) {
+        try {
+            //todo: getBlockByNumber
+            BlockMock blockMock = new BlockMock(nodeManager);
+            return blockMock.retBlockMock();
+        } catch (Exception exception) {
+            throw new NonExistObjectException("block");
+        }
     }
 
     @Override
     public int newBlockFilter() {
-        return 0;
+        try {
+            return 0;
+        } catch (Exception exception) {
+            throw new InternalErrorException();
+        }
     }
 }
 
