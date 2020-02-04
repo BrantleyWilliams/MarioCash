e8b78ee22268810027585ff0cbc3579e5e73e997
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2020-02-04 04:20:54 +0000

    Fix build failed (CheckStyle)

--- mariocash-node/src/main/java/io/mariocash/node/api/PeerApi.java
+++ mariocash-node/src/main/java/io/mariocash/node/api/PeerApi.java
@@ -4,8 +4,8 @@ import com.googlecode.jsonrpc4j.JsonRpcError;
 import com.googlecode.jsonrpc4j.JsonRpcErrors;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 import com.googlecode.jsonrpc4j.JsonRpcService;
-import dev.zhihexireng.core.net.Peer;
 import dev.zhihexireng.core.exception.NonExistObjectException;
+import dev.zhihexireng.core.net.Peer;
 
 import java.util.Collection;
 import java.util.List;
