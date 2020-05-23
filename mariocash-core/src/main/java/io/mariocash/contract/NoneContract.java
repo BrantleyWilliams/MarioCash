fc039b576130cf3707a534487ddb56ac33397286
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2020-05-23 12:50:49 +0000

    Change the parameter type of query methods as JsonArray on StemContract and add query test to RuntimeTest

--- mariocash-core/src/main/java/io/mariocash/contract/NoneContract.java
+++ mariocash-core/src/main/java/io/mariocash/contract/NoneContract.java
@@ -2,6 +2,7 @@ package dev.zhihexireng.contract;
 
 import com.google.gson.JsonObject;
 import dev.zhihexireng.core.TransactionHusk;
+import dev.zhihexireng.core.store.StateStore;
 import dev.zhihexireng.core.store.TransactionReceiptStore;
 
 public class NoneContract implements Contract {
---
+++ mariocash-node/src/main/java/io/mariocash/node/api/BranchApiImpl.java
@@ -0,0 +1,39 @@
+package dev.zhihexireng.node.api;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import dev.zhihexireng.node.controller.TransactionDto;
+
+import java.util.List;
+
+public class BranchApiImpl implements BranchApi {
+    @Override
+    public String createBranch(TransactionDto tx) {
+        return null;
+    }
+
+    @Override
+    public String updateBranch(TransactionDto tx) {
+        return null;
+    }
+
+    @Override
+    public List<JsonObject> searchBranch(String key, String value) {
+        return null;
+    }
+
+    @Override
+    public String viewBranch(String branchId) {
+        return null;
+    }
+
+    @Override
+    public String getCurrentVersionOfBranch(String branchId) {
+        return null;
+    }
+
+    @Override
+    public JsonArray getVersionHistoryOfBranch(String branchId) {
+        return null;
+    }
+}
