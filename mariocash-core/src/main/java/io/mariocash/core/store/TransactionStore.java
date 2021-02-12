57edfd778c9a6467fed7a20e70ab278118214413
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2021-02-12 21:45:53 +0000

    bc builder change builder patten

--- mariocash-core/src/main/java/io/mariocash/core/store/TransactionStore.java
+++ mariocash-core/src/main/java/io/mariocash/core/store/TransactionStore.java
@@ -16,7 +16,6 @@
 
 package dev.zhihexireng.core.store;
 
-import com.google.common.collect.EvictingQueue;
 import dev.zhihexireng.common.Sha3Hash;
 import dev.zhihexireng.core.TransactionHusk;
 import dev.zhihexireng.core.exception.FailedOperationException;
@@ -27,26 +26,25 @@ import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
 
 public class TransactionStore implements Store<Sha3Hash, TransactionHusk> {
     private static final Logger log = LoggerFactory.getLogger(TransactionStore.class);
     private static final Lock LOCK = new ReentrantLock();
 
-    private int cacheSize = 500;
+    private long cacheSize = 500;
     private long countOfTxs = 0;
 
     private final DbSource<byte[], byte[]> db;
     private final Cache<Sha3Hash, TransactionHusk> huskTxPool;
-    private Queue<TransactionHusk> recentTxs;
+    private final TreeSet<TransactionHusk> recentTxs = new TreeSet<>();
     private final Set<Sha3Hash> unconfirmedTxs = new HashSet<>();
 
     TransactionStore(DbSource<byte[], byte[]> db) {
@@ -56,17 +54,15 @@ public class TransactionStore implements Store<Sha3Hash, TransactionHusk> {
                 .createCache("txPool", CacheConfigurationBuilder
                         .newCacheConfigurationBuilder(Sha3Hash.class, TransactionHusk.class,
                                 ResourcePoolsBuilder.heap(Long.MAX_VALUE)));
-        this.recentTxs = EvictingQueue.create(this.cacheSize);
-
     }
 
-    TransactionStore(DbSource<byte[], byte[]> db, int cacheSize) {
+    TransactionStore(DbSource<byte[], byte[]> db, long cacheSize) {
         this(db);
-        this.recentTxs = EvictingQueue.create(cacheSize);
+        this.cacheSize = cacheSize;
     }
 
     public Collection<TransactionHusk> getRecentTxs() {
-        return recentTxs.stream().collect(Collectors.toCollection(ArrayList::new));
+        return this.recentTxs;
     }
 
     @Override
@@ -123,6 +119,9 @@ public class TransactionStore implements Store<Sha3Hash, TransactionHusk> {
 
     private void addReadCache(TransactionHusk tx) {
         recentTxs.add(tx);
+        if (recentTxs.size() > cacheSize) {
+            this.recentTxs.pollFirst();
+        }
     }
 
     public long countOfTxs() {
