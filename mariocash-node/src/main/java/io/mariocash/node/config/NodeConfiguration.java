c4dc54bb6e7dff7ae52dd162d6cd0cde6d7055a1
Mario Reng <68059652+zhihexireng@users.noreply.github.com>
2021-08-10 04:57:19 +0000

    Remove unnecessary

--- mariocash-node/src/main/java/io/mariocash/node/config/NodeConfiguration.java
+++ mariocash-node/src/main/java/io/mariocash/node/config/NodeConfiguration.java
@@ -24,7 +24,6 @@ import dev.zhihexireng.core.net.PeerGroup;
 import dev.zhihexireng.node.config.annotaion.EnableDefaultBranch;
 import org.spongycastle.crypto.InvalidCipherTextException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -61,7 +60,6 @@ public class NodeConfiguration {
     }
 
     @Bean
-    @Autowired
     Wallet wallet(DefaultConfig defaultConfig) throws IOException, InvalidCipherTextException {
         return new Wallet(defaultConfig);
     }
